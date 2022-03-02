f1e42c4c06a16aa09f6eec5e7d6d5b94f675f3dc
mingii4922 <chlalsrl98@naver.com>
2022-03-02 23:16:52 +0900

    commit model.py.

--- model_transformer.py
+++ model_transformer.py
@@ -79,38 +79,52 @@ class Transformer(nn.Module):
             x = ff(x) + x
         return x
 
-
-class PatchEmbedding(nn.Module):
+class PatchEmbedding(nn.Module): # 이건 conv로 하는거
     def __init__(self, in_channels: int = 1, patch_size: int = 16, emb_size: int = 256, img_size: int = 256):
         self.patch_size = patch_size
         super().__init__()
-        self.projection = nn.Sequential(
-            # using a conv layer instead of a linear one -> performance gains
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
-            Rearrange('b e (h) (w) -> b (h w) e'),
+
+        # self.projection = nn.Sequential(
+        #     # using a conv layer instead of a linear one -> performance gains
+        #     nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+        #     Rearrange('b e (h) (w) -> b (h w) e'),
+        # )
+        ### 이게 patch로 linear하는거
+        # self.to_patch_embedding = nn.Sequential(
+        #     Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+        #     nn.Linear(256, dim),
+        # )
+
+        ### 이게 patch로 conv하는거
+        self.to_patch_embedding = nn.Sequential(
+            nn.Conv2d(1,emb_size,patch_size,patch_size), # rfft : 1,256,16,8
+            Rearrange('b e (h) (w) -> b (h w) e'), # 1,128,256
         )
-        # self.positions = nn.Parameter(torch.randn((img_size // patch_size) ** 2, emb_size))
-        # self.positions = nn.Parameter(torch.randn(img_size, emb_size))
-        # self.lin = nn.Sequential(
-        #     Rearrange('b (e) h (w) -> b h (e w)'),
-        #     nn.Linear(img_size//2+1, emb_size))
-        self.positions = nn.Parameter(torch.randn(img_size//2+1, emb_size))
-        self.lin = nn.Sequential(
-            Rearrange('b (e) (h) w -> b w (e h)'),
-            nn.Linear(img_size, emb_size))
+
+        ### 이게 flatten linear하는거
+        # self.to_patch_embedding = nn.Sequential(
+        #     Rearrange('b (c) (h) w -> b w (c h)'),
+        #     nn.Linear(img_size, emb_size))
+
+
+        ## rfft patch로 했을 때(linear, conv)
+        self.pos_embedding = nn.Parameter(torch.randn(1,img_size//2,emb_size)) # 128,256
+
+        ## rfft flatten 세로
+        # self.pos_embedding = nn.Parameter(torch.randn(1,img_size,dim)) # 256, 256
+
+        ## fft patch로 했을 때
+        # self.pos_embedding = nn.Parameter(torch.randn((img_size // patch_size) ** 2, emb_size))
+
     def forward(self, x):
         b, _, _, _ = x.shape
-        # x = self.projection(x)
-        #
-        # x += self.positions
 
-        x = abs(torch.fft.rfft2(x, dim=(2, 3), norm='ortho')) # b, 1, 256, 256 -> b, 1, 256, 129
-        x = self.lin(x)
-        x += self.positions
+        x = self.to_patch_embedding(x) # b,256,129*1
+        x += self.pos_embedding
         return x
 
 class Rfft_mymodel(nn.Module):
-    def __init__(self, enet_type=None, out_dim=4, img_size=256, patch_size=16, n_classes=4, dim=256, depth=12, heads=12, mlp_dim=256, pool = 'cls', channels = 1, dim_head = 64, dropout = 0., emb_dropout = 0.):
+    def __init__(self, enet_type=None, out_dim=4, img_size=256, patch_size=16, n_classes=1000, dim=256, depth=12, heads=12, mlp_dim=256, pool = 'cls', channels = 1, dim_head = 64, dropout = 0., emb_dropout = 0.):
         super(Rfft_mymodel,self).__init__()
         ##################################################################################################################################
         image_height, image_width = pair(img_size)
@@ -126,14 +140,10 @@ class Rfft_mymodel(nn.Module):
         num_patches = (image_height // patch_height) * (image_width // patch_width) # patch 개수
         patch_dim = channels * patch_height * patch_width # 1 * 16 * 16,  patch하나에 들어있는 값?
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
-        # self.to_patch_embedding = PatchEmbedding2(in_channels=channels,patch_size=patch_size,emb_size=dim,img_size=img_size)
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
-            nn.Linear(patch_dim, dim),
-        )
-        #
-        # # self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
+        ## 이게 flatten으로 하는거
+        self.to_patch_embedding = PatchEmbedding(in_channels=channels,patch_size=patch_size,emb_size=dim,img_size=img_size)
+
+        ## transformer 들어가기 전
         self.dropout = nn.Dropout(emb_dropout) # transformer 들어가기 전
 
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
@@ -153,37 +163,38 @@ class Rfft_mymodel(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(128, out_dim))
 
-        self.preconv1 = nn.Sequential(
-            nn.Conv2d(channels, 32, kernel_size=3, stride=1, padding=1),
-            nn.LayerNorm(img_size),
-            nn.GELU(),
-        )
-        self.preconv2 = nn.Sequential(
-            nn.Conv2d(32, channels, kernel_size=3, stride=1, padding=1),
-            # nn.LayerNorm(img_size)
-        )
-    #     self.const_weight = nn.Parameter(torch.randn(size=[1, 1, 5, 5]), requires_grad=True)
-    #
-    # def normalized_F(self):
-    #     central_pixel = (self.const_weight.data[:, 0, 2, 2])
-    #     for i in range(1):
-    #         sumed = self.const_weight.data[i].sum() - central_pixel[i]
-    #         self.const_weight.data[i] /= sumed
-    #         self.const_weight.data[i, 0, 2, 2] = -1.0
+        # self.preconv1 = nn.Sequential(
+        #     nn.Conv2d(channels, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.LayerNorm(img_size),
+        #     nn.GELU(),
+        # )
+        # self.preconv2 = nn.Sequential(
+        #     nn.Conv2d(32, channels, kernel_size=3, stride=1, padding=1),
+        #     # nn.LayerNorm(img_size)
+        # )
+        #     self.const_weight = nn.Parameter(torch.randn(size=[1, 1, 5, 5]), requires_grad=True)
+        #
+        # def normalized_F(self):
+        #     central_pixel = (self.const_weight.data[:, 0, 2, 2])
+        #     for i in range(1):
+        #         sumed = self.const_weight.data[i].sum() - central_pixel[i]
+        #         self.const_weight.data[i] /= sumed
+        #         self.const_weight.data[i, 0, 2, 2] = -1.0
     def forward(self, img):
         # self.normalized_F()
         # img = F.conv2d(img, self.const_weight,padding=2)
-        img = torch.abs(fft.rfft2(img, dim=(2, 3), norm='ortho'))
-        img = img[:,:,:,:-1]
         # x = img + self.preconv2(self.preconv1(img))
+        img = torch.abs(fft.rfft2(img, dim=(2, 3), norm='ortho')) # b,1,256,129
+        img = img[:,:,:,:-1] # b,1,256,128
 
         x = self.to_patch_embedding(img)
         b, n, _ = x.shape
 
-        # x += self.pos_embedding[:, :(n)]
+        ##########여기 밑은 고정하고 하자..!!################
         x = self.dropout(x)
 
         x = self.transformer(x)
+
         x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
 
         x = self.to_latent(x)
